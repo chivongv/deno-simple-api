c4991738f8bca547d0f7f25ef214ada55f3618fb
Chi Vong <chivongv@gmail.com>
2021-02-09 08:11:19 +0100

    ✨ Add https for localhost

--- server.ts
+++ server.ts
@@ -7,7 +7,12 @@ const port = Number(Deno.env.get("port")) || 5000;
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen({ port });
-console.log(`Server is running on http://localhost:${port}`);
+app.listen({
+  port,
+  secure: true,
+  certFile: "tls/localhost.crt",
+  keyFile: "tls/localhost.key",
+});
+console.log(`Server is running on https://localhost:${port}`);
 
 export default app;
